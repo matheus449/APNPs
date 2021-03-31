9c30b3be1f460a096cb19cee1b75cd861df7c5bc
Matheus <mt449matheus@gmail.com>
2021-03-30 21:07:45 -0300

    configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require ('express')
 const app = express ()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use (cookieParser())
+
+app.use (bodyParser.json())
+app.use (bodyParser.urlencoded({extended:false}))
+
+app.set("view engine", "ejs")
+
+app.use (express.static(path.join(__dirname, "public")))
+
+
 
 app.get ('/', function(req,res){
     res.send("Bem vindo Matheus")
